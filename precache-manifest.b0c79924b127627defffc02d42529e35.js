f30e5b2aaffc10b5612232bc9e4a49c94f393096
gong zhaopeng <gongzhaopeng@gongs-MacBook-Air.local>
2020-02-07 00:01:07 +0800

    Updates

---
+++ precache-manifest.b0c79924b127627defffc02d42529e35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0cb113eab99066d091f9a95eba2e8fb",
+    "url": "/avatar/index.html"
+  },
+  {
+    "revision": "2d1b74014c7ca316a6ec",
+    "url": "/avatar/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cca50a86a450ff114bb6",
+    "url": "/avatar/static/js/2.3207ec71.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/avatar/static/js/2.3207ec71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d1b74014c7ca316a6ec",
+    "url": "/avatar/static/js/main.40c8586f.chunk.js"
+  },
+  {
+    "revision": "0df6c8da4d7d1ff173f1",
+    "url": "/avatar/static/js/runtime-main.5f91fb84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/avatar/static/media/logo.5d5d9eef.svg"
+  }
+]);
