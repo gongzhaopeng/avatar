74f773946fc64f0724d15c7e4600fc32dcf69025
gong zhaopeng <gongzhaopeng@gongs-MacBook-Air.local>
2020-02-07 23:07:07 +0800

    ...

--- src/components/Task.js
+++ src/components/Task.js
@@ -19,7 +19,7 @@ export default function Task(
                 />
                 <span className="checkbox-custom" onClick={() => onArchiveTask(id)}/>
             </label>
-            <div className="title">
+            <div className="title" style={{background: "red"}}>
                 <input type="text" value={title} readOnly={true} placeholder="Input title"/>
             </div>
 
