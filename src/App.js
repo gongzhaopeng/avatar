2df3350c114a3b83a54ecc6315d8398ada145c94
gong zhaopeng <gongzhaopeng@gongs-MacBook-Air.local>
2020-02-09 11:29:08 +0800

    ...

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React, {Component} from 'react';
-import {Provider} from 'react-redux';
-import store from './lib/redux';
-
-import InboxScreen from './components/InboxScreen';
 
 class App extends Component {
     render() {
-        return (
-            <Provider store={store}>
-                <InboxScreen/>
-            </Provider>
-        );
+        return <h1 color="red">PLACEHOLDER</h1>;
     }
 }
 
