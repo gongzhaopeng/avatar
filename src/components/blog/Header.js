9d7f12f0fc8f675611d6e2a441890d78f33d0e81
gong zhaopeng <gongzhaopeng@gongs-MacBook-Air.local>
2020-02-17 20:34:16 +0800

    ...

--- src/components/blog/Header.js
+++ src/components/blog/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
@@ -9,66 +9,79 @@ import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles(theme => ({
-  toolbar: {
-    borderBottom: `1px solid ${theme.palette.divider}`,
-  },
-  toolbarTitle: {
-    flex: 1,
-  },
-  toolbarSecondary: {
-    justifyContent: 'space-between',
-    overflowX: 'auto',
-  },
-  toolbarLink: {
-    padding: theme.spacing(1),
-    flexShrink: 0,
-  },
+    toolbar: {
+        borderBottom: `1px solid ${theme.palette.divider}`,
+    },
+    toolbarTitle: {
+        flex: 1,
+    },
+    toolbarSecondary: {
+        justifyContent: 'space-between',
+        overflowX: 'auto',
+    },
+    toolbarLink: {
+        padding: theme.spacing(1),
+        flexShrink: 0,
+    },
 }));
 
-export default function Header(props) {
-  const classes = useStyles();
-  const { sections, title } = props;
+function githubAuthUrl() {
+    const url = new URL('https://github.com/login/oauth/authorize');
 
-  return (
-    <React.Fragment>
-      <Toolbar className={classes.toolbar}>
-        <Button size="small">Subscribe</Button>
-        <Typography
-          component="h2"
-          variant="h5"
-          color="inherit"
-          align="center"
-          noWrap
-          className={classes.toolbarTitle}
-        >
-          {title}
-        </Typography>
-        <IconButton>
-          <SearchIcon />
-        </IconButton>
-        <Button variant="outlined" size="small">
-          Sign up
-        </Button>
-      </Toolbar>
-      <Toolbar component="nav" variant="dense" className={classes.toolbarSecondary}>
-        {sections.map(section => (
-          <Link
-            color="inherit"
-            noWrap
-            key={section.title}
-            variant="body2"
-            href={section.url}
-            className={classes.toolbarLink}
-          >
-            {section.title}
-          </Link>
-        ))}
-      </Toolbar>
-    </React.Fragment>
-  );
+    const searchParams = url.searchParams;
+    searchParams.append('client_id', process.env.REACT_APP_GITHUB_AUTH_CLIENT_ID);
+    searchParams.append('redirect_uri', process.env.REACT_APP_GITHUB_AUTH_REDIRECT_URI);
+    searchParams.append('login', 'gongzhaopeng');
+    searchParams.append('scope', 'user repo');
+    searchParams.append('allow_signup', 'false');
+
+    return url;
+}
+
+export default function Header(props) {
+    const classes = useStyles();
+    const {sections, title} = props;
+
+    return (
+        <React.Fragment>
+            <Toolbar className={classes.toolbar}>
+                <Typography
+                    component="h2"
+                    variant="h5"
+                    color="inherit"
+                    align="center"
+                    noWrap
+                    className={classes.toolbarTitle}
+                >
+                    {title}
+                </Typography>
+                <IconButton>
+                    <SearchIcon/>
+                </IconButton>
+                <Button variant="outlined" size="small"
+                        onClick={() => window.location.href = githubAuthUrl().href}>
+                    TO BE OWNER
+                </Button>
+            </Toolbar>
+            <Toolbar component="nav" variant="dense" className={classes.toolbarSecondary}>
+                {sections.map(section => (
+                    <Link
+                        color="inherit"
+                        noWrap
+                        key={section.title}
+                        variant="body2"
+                        href={section.url}
+                        className={classes.toolbarLink}
+                    >
+                        {section.title}
+                    </Link>
+                ))}
+            </Toolbar>
+        </React.Fragment>
+    );
 }
 
 Header.propTypes = {
-  sections: PropTypes.array,
-  title: PropTypes.string,
+    sections: PropTypes.array,
+    title: PropTypes.string,
 };
